e327446c30df30823872b522589fdd5bf9fbc80b
Malgin Viktor <leesunsinn@protonmail.com>
2021-11-12 17:59:51 +0300

    timer fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -313,14 +313,14 @@ function calc() {
                   break;
           }
         calcTotal();
-      });
+        });
     }
 
     getDynamicInformation('#height');
     getDynamicInformation('#weight');
     getDynamicInformation('#age');
-  }
-  calc();
+}
+calc();
 
 
 // cards
@@ -346,10 +346,10 @@ const menu = [
         descr: "В меню 'Премиум' мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
         price: 320
     }
-  ]
-  
-  function renderCards() {
-  
+]
+
+function renderCards() {
+
     const cards = document.querySelector('#cards');
 
     return (
@@ -366,64 +366,68 @@ const menu = [
         </div>`
         )).join('')
     )
-  }
-  
-  renderCards();
+}
+
+renderCards();
 
 //timer
-  function timer(id, deadline) {
+function timer(id, deadline) {
 
     function getTimeRemaning(endTime) {
-      const t = Date.parse(endTime) - Date.parse(new Date()),
+        const t = Date.parse(endTime) - Date.parse(new Date()),
       // Второй date.parse можно убрать, но оставим элементы идентичными для наглядности.
-        days = Math.floor(t / (1000 * 60 * 60 * 24)),
-        hours = Math.floor((t / (1000 * 60 * 60)) % 24),
-        minutes = Math.floor((t / (1000 * 60)) % 60),
-        seconds = Math.floor((t / 1000) % 60);
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60)) % 24),
+            minutes = Math.floor((t / (1000 * 60)) % 60),
+            seconds = Math.floor((t / 1000) % 60);
 
-      return {
-        'total': t,
-        'days': days,
-        'hours': hours,
-        'minutes': minutes,
-        'seconds': seconds
-      };
-  }
-
-  function getZero(num) {
-    if (num >= 0 && num < 10) {
-      return `0${num}`;
-      } else {
-      return num;
+        return {
+            'total': t,
+            'days': days,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
     }
-  }
 
-  function setClock (selector, endTime) {
-    const timer = document.querySelector(selector),
-      days = timer.querySelector('#days'),
-      hours = timer.querySelector('#hours'),
-      minutes = timer.querySelector('#minutes'),
-      seconds = timer.querySelector('#seconds'),
-      timeInterval = setInterval(updateClock, 1000);
-    updateClock();
-
-    function  updateClock() {
-      const t = getTimeRemaning(endTime);
-
-      days.innerHTML = getZero(t.days);
-      hours.innerHTML = getZero(t.hours);
-      minutes.innerHTML = getZero(t.minutes);
-      seconds.innerHTML = getZero(t.seconds);
-
-      if (t.total <= 0) {
-        clearInterval(timeInterval);
-      }
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+        return `0${num}`;
+        } else {
+        return num;
+        }
     }
-  }
 
-  setClock(id, deadline);
-  }
-  timer('.timer', '2022-08-11');
+    function setClock (selector, endTime) {
+        const timer = document.querySelector(selector),
+        days = timer.querySelector('#days'),
+        hours = timer.querySelector('#hours'),
+        minutes = timer.querySelector('#minutes'),
+        seconds = timer.querySelector('#seconds'),
+        timeInterval = setInterval(updateClock, 1000);
+        updateClock();
+
+        function  updateClock() {
+            const t = getTimeRemaning(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                days.textContent = "00";
+                hours.textContent = "00";
+                minutes.textContent = "00";
+                seconds.textContent = "00";
+                clearInterval(timeInterval);
+            }
+        }
+    }
+setClock(id, deadline);
+}
+
+timer('.timer', '2021-08-11');
 
 // modal
 function closeModal(modalSelector) {
@@ -488,17 +492,17 @@ const body = document.querySelector('body'),
     hamburger = document.querySelector('.hamburger');
 
 hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("hamburger_active");
-  headerMenu.classList.toggle('header__links_active');
-  document.body.classList.toggle('fixed');
+    hamburger.classList.toggle("hamburger_active");
+    headerMenu.classList.toggle('header__links_active');
+    document.body.classList.toggle('fixed');
 });
 
 menuItem.forEach(item => {
-  item.addEventListener('click', () => {
-    hamburger.classList.toggle('hamburger_active');
-    headerMenu.classList.toggle('header__links_active');
-    body.classList.toggle('fixed');
-  })
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        headerMenu.classList.toggle('header__links_active');
+        body.classList.toggle('fixed');
+    })
 })
 
 // cделать завершающее окно заглушкой - по заполнении полей и нажатии кнопки
